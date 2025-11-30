62638d85ba579c9f71743b765cde5d4d88638c62
mddrm6 <keepo2006@mail.ru>
2025-11-30 12:57:45 +0300

    5 types added to types.ts Jmerenchuk

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,57 @@
-export type sum = (a: number, b: number) => number
+export type ValidatorMessage = string;
+
+export type ValidatorError = {
+    fieldsName: string,
+    messages: ValidatorMessage
+}
+
+export type ValidationResult = {
+    isOk: boolean,
+    errors: ValidatorError[],
+    values?: Record<string, any>
+}
+
+
+export interface BaseChain {
+    required(message?: ValidatorMessage): this,
+
+    custom(
+        validator: (raw: any) => boolean,
+        message?: ValidatorMessage
+    ): this,
+}
+
+export interface StringChain extends BaseChain {
+    min(
+        length: number,
+        message?: ValidatorMessage
+    ): this,
+    
+    max(
+        length: number,
+        message?: ValidatorMessage
+    ): this,
+    
+    pattern(
+        regex: RegExp,
+        message?: ValidatorMessage
+    ): this,
+    
+    trim(): this
+}
+
+export interface NumberChain extends BaseChain {
+    min(
+        value: number,
+        message?: ValidatorMessage
+    ): this,
+    
+    max(
+        value: number,
+        message?: ValidatorMessage
+    ): this,
+    
+    integer(
+        message?: string
+    ): this
+}
